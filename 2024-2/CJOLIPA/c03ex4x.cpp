d5185d12161ebb5508dbd8d3baaf789310114150
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-09-05 17:36:52 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/c03ex4x.cpp
@@ -0,0 +1,32 @@
+// Baseado no exercício 4X, capítulo 3 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+    setlocale(LC_ALL, ".1252");
+
+    float base, expoente, indice, raiz;
+
+    std::cout << "Informe a base: "; std::cin >> base;
+    std::cin.ignore(80, '\n');
+    std::cout << std::endl;
+
+    std::cout << "Informe o expoente: "; std::cin >> expoente;
+    std::cin.ignore(80, '\n');
+    std::cout << std::endl;
+
+    std::cout << "Informe o índice: "; std::cin >> indice;
+    std::cin.ignore(80, '\n');
+    std::cout << std::endl;
+
+    raiz = pow(base, (expoente / indice));
+
+    std::cout << "Raiz obtida = " << raiz << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
