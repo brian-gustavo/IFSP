1617cd7ea1256445c0f8c26e1e40d04d274dc240
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-09-15 22:39:57 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/c03ex4k.cpp
@@ -0,0 +1,27 @@
+// Baseado no exercício 4K, capítulo 3 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+
+int main(void)
+{
+    setlocale(LC_ALL, ".1252");
+
+    float carteira, cotacao, conversao;
+
+    std::cout << "Informe a quantidade de dólares que você possui: "; std::cin >> carteira;
+    std::cin.ignore(80, '\n');
+    std::cout << std::endl;
+
+    std::cout << "Informe a atual cotação do dólar: "; std::cin >> cotacao;
+    std::cin.ignore(80, '\n');
+    std::cout << std::endl;
+
+    conversao = carteira * cotacao;
+
+    std::cout << "Conversão em reais = " << conversao << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
