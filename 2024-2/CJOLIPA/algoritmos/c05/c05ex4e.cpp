68ce2bbfc7d50449d7d50841f71a5d25533d9a4b
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-09 14:37:12 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex4e.cpp
@@ -0,0 +1,22 @@
+// Baseado no exercício 4E, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+
+int main(void)
+{
+    int resto, numero = 0;
+
+    do
+        {
+            resto = numero - 2 * (numero / 2);
+            if (resto != 0)
+                std::cout << numero << std::endl;
+            numero++;
+        }
+    while (not(numero > 20));
+
+    std::cout << std::endl;
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
