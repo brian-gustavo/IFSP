0465338df18b3c417b2e7aa610014d88c3e80b11
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-16 16:31:42 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex4m.cpp
@@ -0,0 +1,31 @@
+// Baseado no exercício 4M, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+
+int main(void)
+{
+    setlocale(LC_ALL, ".1252");
+
+    float valor, media, soma = 0;
+    int contador = 1;
+
+    do
+        {
+            std::cout << "Insira um valor: "; std::cin >> valor;
+            std::cin.ignore(80, '\n');
+            std::cout << std::endl;
+
+            soma += valor;
+            contador++;
+        }
+    while (not(contador > 10));
+
+    media = soma / 10;
+
+    std::cout << "Somatório dos valores = " << soma << std::endl;
+    std::cout << "Média dos valores = " << media << std::endl;
+
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
