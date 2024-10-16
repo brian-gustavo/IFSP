ada238cb8b96efea69419767fbd159dea7701988
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-16 16:03:02 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex4a.cpp
@@ -0,0 +1,21 @@
+// Baseado no exercício 4A, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+    int quadrado, inteiro = 15;
+
+    do
+        {
+            quadrado = pow(inteiro, 2);
+            std::cout << quadrado << std::endl << std::endl;
+            inteiro++;
+        }
+    while (not(inteiro > 200));
+
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
