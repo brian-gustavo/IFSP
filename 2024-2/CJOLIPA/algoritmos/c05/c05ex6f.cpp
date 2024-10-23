a98e315eb3faa97435d4b714f1b454f60eebaf40
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-23 17:33:15 -0300

    Add file via upload

---
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex6f.cpp
@@ -0,0 +1,20 @@
+// Baseado no exercício 6F, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+
+#include <iostream>
+
+int main(void)
+{
+    int resto;
+
+    for (int numero = 1; numero <= 199; ++numero)
+        {
+            resto = numero - 4 * (numero / 4);
+            if (resto == 0)
+                std::cout << numero << std::endl;
+        }
+
+    std::cout << std::endl;
+    std::cout << "Tecle <Enter> para encerrar... ";
+    std::cin.get();
+    return 0;
+}
