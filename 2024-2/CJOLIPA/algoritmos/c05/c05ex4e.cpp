cb5ebfa4121d94e38c2dd2436e1ee446d8958f3c
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-24 15:54:14 -0300

    Update c05ex4e.cpp

--- 2024-2/CJOLIPA/algoritmos/c05/c05ex4e.cpp
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex4e.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 4E, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 4E, capÃ­tulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -7,12 +7,12 @@ int main(void)
     int resto, numero = 0;
 
     do
-        {
-            resto = numero - 2 * (numero / 2);
-            if (resto != 0)
-                std::cout << numero << std::endl;
-            numero++;
-        }
+    {
+        resto = numero - 2 * (numero / 2);
+        if (resto != 0)
+            std::cout << numero << std::endl;
+        numero++;
+    }
     while (not(numero > 20));
 
     std::cout << std::endl;
