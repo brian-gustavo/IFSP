c3b2344f2dd583340fef792fd94b067806f65c26
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-24 15:52:35 -0300

    Update c05ex1e.cpp

--- 2024-2/CJOLIPA/algoritmos/c05/c05ex1e.cpp
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex1e.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 1E, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 1E, capÃ­tulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -7,12 +7,12 @@ int main(void)
     int resto, numero = 0;
 
     while (numero <= 20)
-        {
-            resto = numero - 2 * (numero / 2);
-            if (resto != 0)
-                std::cout << numero << std::endl;
-            numero++;
-        }
+    {
+        resto = numero - 2 * (numero / 2);
+        if (resto != 0)
+            std::cout << numero << std::endl;
+        numero++;
+    }
 
     std::cout << std::endl;
     std::cout << "Tecle <Enter> para encerrar... ";
