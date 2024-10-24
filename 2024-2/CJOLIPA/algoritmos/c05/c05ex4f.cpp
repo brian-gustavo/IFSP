aa1ed06e0339405ee5b1b3c57fea5306964cd3f4
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-10-24 15:54:32 -0300

    Update c05ex4f.cpp

--- 2024-2/CJOLIPA/algoritmos/c05/c05ex4f.cpp
+++ 2024-2/CJOLIPA/algoritmos/c05/c05ex4f.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 4F, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 4F, capÃ­tulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -7,12 +7,12 @@ int main(void)
     int resto, numero = 1;
 
     do
-        {
-            resto = numero - 4 * (numero / 4);
-            if (resto == 0)
-                std::cout << numero << std::endl;
-            numero++;
-        }
+    {
+        resto = numero - 4 * (numero / 4);
+        if (resto == 0)
+            std::cout << numero << std::endl;
+        numero++;
+    }
     while (not(numero > 200));
 
     std::cout << std::endl;
