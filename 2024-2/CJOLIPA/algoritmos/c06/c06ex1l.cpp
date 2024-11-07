69eee5eec49e49255fa8fa8635cea18a2543865b
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-07 17:39:00 -0300

    Update c06ex1l.cpp

--- 2024-2/CJOLIPA/algoritmos/c06/c06ex1l.cpp
+++ 2024-2/CJOLIPA/algoritmos/c06/c06ex1l.cpp
@@ -7,24 +7,24 @@ int main(void)
     int contador;
     float a[10], b[10];
 
-    for (contador = 1; contador <= 10; ++contador)
+    for (contador = 0; contador <= 9; ++contador)
     {
-        std::cout << "Informe o " << contador << "o. valor da matriz: ";
+        std::cout << "Informe o " << contador + 1 << "o. valor da matriz: ";
         std::cin >> a[contador];
         std::cin.ignore(80, '\n');
     }
     std::cout << std::endl;
 
-    for (contador = 1; contador <= 10; ++contador)
+    for (contador = 0; contador <= 9; ++contador)
         b[contador] = a[contador] / 2;
 
     std::cout << "Elementos da primeira matriz: " << std::endl;
-    for (contador = 1; contador <= 10; ++contador)
+    for (contador = 0; contador <= 9; ++contador)
         std::cout << a[contador] << std::endl;
     std::cout << std::endl;
 
     std::cout << "Elementos da segunda matriz: " << std::endl;
-    for (contador = 1; contador <= 10; ++contador)
+    for (contador = 0; contador <= 9; ++contador)
         std::cout << b[contador] << std::endl;
     std::cout << std::endl;
 
