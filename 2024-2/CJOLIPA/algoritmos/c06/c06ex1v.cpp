ea0ea7feac8a531ed439acd82ca8f844a7d10e42
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-07 17:40:20 -0300

    Update c06ex1v.cpp

--- 2024-2/CJOLIPA/algoritmos/c06/c06ex1v.cpp
+++ 2024-2/CJOLIPA/algoritmos/c06/c06ex1v.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 1V, capítulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 1V, capÃ­tulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -9,14 +9,14 @@ int main(void)
     int contador, resto, totalpares = 0, totalimpares = 0;
     int a[30];
 
-    for (contador = 1; contador <= 30; ++contador)
+    for (contador = 0; contador <= 29; ++contador)
     {
-        std::cout << "Informe o " << contador << "o. valor da matriz: ";
+        std::cout << "Informe o " << contador + 1 << "o. valor da matriz: ";
         std::cin >> a[contador];
         std::cin.ignore(80, '\n');
     }
 
-    for (contador = 1; contador <= 30; ++contador)
+    for (contador = 0; contador <= 29; ++contador)
     {
         resto = a[contador] - 2 * (a[contador] / 2);
         if (resto == 0)
@@ -27,7 +27,7 @@ int main(void)
 
     std::cout << std::endl;
     std::cout << "Total de valores pares presentes na matriz = " << totalpares << std::endl;
-    std::cout << "Total de valores ímpares presentes na matriz = " << totalimpares << std::endl;
+    std::cout << "Total de valores Ã­mpares presentes na matriz = " << totalimpares << std::endl;
 
     std::cout << std::endl;
     std::cout << "Tecle <Enter> para encerrar... ";
