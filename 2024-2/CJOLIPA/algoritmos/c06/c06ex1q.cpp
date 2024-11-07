18f3ea2540ebfe864d9e29af46eff8f9eba34dd0
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-07 17:39:44 -0300

    Update c06ex1q.cpp

--- 2024-2/CJOLIPA/algoritmos/c06/c06ex1q.cpp
+++ 2024-2/CJOLIPA/algoritmos/c06/c06ex1q.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 1Q, capítulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 1Q, capÃ­tulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -7,14 +7,14 @@ int main(void)
     int contador, resto;
     float a[15], b[15];
 
-    for (contador = 1; contador <= 15; ++contador)
+    for (contador = 0; contador <= 14; ++contador)
     {
-        std::cout << "Informe o " << contador << "o. valor da matriz: ";
+        std::cout << "Informe o " << contador + 1 << "o. valor da matriz: ";
         std::cin >> a[contador];
         std::cin.ignore(80, '\n');
     }
 
-    for (contador = 1; contador <= 15; ++contador)
+    for (contador = 0; contador <= 14; ++contador)
     {
         resto = contador - 2 * (contador / 2);
         if (resto == 0)
@@ -24,9 +24,9 @@ int main(void)
     }
 
     std::cout << std::endl;
-    for (contador = 1; contador <= 15; ++contador)
+    for (contador = 0; contador <= 14; ++contador)
     {
-        std::cout << contador << "o. valor da nova matriz = " << b[contador] << std::endl;
+        std::cout << contador + 1 << "o. valor da nova matriz = " << b[contador] << std::endl;
     }
 
     std::cout << std::endl;
