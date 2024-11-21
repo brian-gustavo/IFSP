a95af0ab81b6703b9e2896e1848dfb2a15227224
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-21 17:49:49 -0300

    Update and rename 2024-2/CJOLIPA/algoritmos/c05/c05ex6d.cpp to 2024-2/CJOLIPA/c05/c05ex6d.cpp

--- 2024-2/CJOLIPA/c05/c05ex6d.cpp
+++ 2024-2/CJOLIPA/c05/c05ex6d.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 6D, capítulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 6D, capÃ­tulo 5 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
@@ -15,7 +15,7 @@ int main(void)
             soma += contador;
     }
 
-    std::cout << "Somatório dos valores pares entre 1 e 500 = " << soma;
+    std::cout << "SomatÃ³rio dos valores pares entre 1 e 500 = " << soma;
     std::cout << std::endl;
 
     std::cout << std::endl;
