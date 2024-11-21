a0391368454d78a9f1522f53cca9f5a77de39964
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-21 17:38:06 -0300

    Update and rename 2024-2/CJOLIPA/algoritmos/c10/c10ex2o.cpp to 2024-2/CJOLIPA/c10/c10ex2o.cpp

--- 2024-2/CJOLIPA/c10/c10ex2o.cpp
+++ 2024-2/CJOLIPA/c10/c10ex2o.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 2O, capítulo 10 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 2O, capÃ­tulo 10 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 
