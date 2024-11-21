01743acac76b80fb8673519c95089d2740938b47
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2024-11-21 17:39:14 -0300

    Update and rename 2024-2/CJOLIPA/algoritmos/c06/c06ex1a.cpp to 2024-2/CJOLIPA/c06/c06ex1a.cpp

--- 2024-2/CJOLIPA/c06/c06ex1a.cpp
+++ 2024-2/CJOLIPA/c06/c06ex1a.cpp
@@ -1,4 +1,4 @@
-// Baseado no exercício 1A, capítulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
+// Baseado no exercÃ­cio 1A, capÃ­tulo 6 do livro "Algoritmos", escrito pelo prof. Augusto Manzano.
 
 #include <iostream>
 #include <string>
