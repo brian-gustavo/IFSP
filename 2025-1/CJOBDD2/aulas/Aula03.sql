6c241e908e0fde070f03dba86fb843499c3818a6
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-03-31 19:49:24 -0300

    Update Aula03.sql

--- 2025-1/CJOBDD2/aulas/Aula03.sql
+++ 2025-1/CJOBDD2/aulas/Aula03.sql
@@ -33,3 +33,24 @@ SELECT F.ID AS 'Código do Funcionário',
 	   D.DataNascimento AS 'Data de Nascimento'
 FROM FUNCIONARIOS F CROSS JOIN DEPENDENTES D;
 GO
+
+SELECT F.ID AS 'Código do Funcionário',
+	   F.Nome AS 'Nome do Funcionário',
+	   F.Salario AS 'Salário',
+	   D.Nome AS 'Nome do Dependente',
+	   D.ID AS 'Código do Responsável'
+FROM FUNCIONARIOS F JOIN DEPENDENTES D;
+	ON F.ID = D.ID;
+GO
+
+SELECT F.ID AS 'Código do Funcionário',
+	   F.Nome AS 'Nome do Funcionário',
+	   F.Salario AS 'Salário',
+	   D.Nome AS 'Nome do Dependente',
+	   D.DataNascimento AS 'Data de Nascimento',
+	   D.ID AS 'Código do Responsável'
+FROM FUNCIONARIOS F INNER JOIN DEPENDENTES D
+	ON F.ID = D.ID
+WHERE YEAR(D.DataNascimento) >= 2000
+ORDER BY F.Name, D.Name;
+GO
