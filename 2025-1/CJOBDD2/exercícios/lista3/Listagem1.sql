283661ed9e0578d3757f59ffd0a0d728c17113b1
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-04-13 19:17:53 -0300

    Create Listagem1.sql

---
+++ 2025-1/CJOBDD2/exercícios/lista3/Listagem1.sql
@@ -0,0 +1,28 @@
+-- Habilita o contexto do banco LojaIFSP, da lista 2
+IF DB_ID (N'LojaIFSP') IS NULL
+  PRINT 'O banco de dados LojaIFSP não existe!';
+ELSE
+  USE LojaIFSP;
+GO
+
+-- Exibe informações de tabelas já existentes no banco de dados atual
+SELECT create_date AS 'Data de Criação',
+       name AS 'Nome da Tabela'
+FROM sys.tables;
+GO
+
+-- Exibe informações sobre a estrutura de uma tabela
+SELECT TABLE_CATALOG AS 'Banco de Dados',
+       TABLE_NAME AS 'Tabela',
+       ORDINAL_POSITION AS 'Posição',
+       COLUMN_NAME AS 'Coluna',
+       DATA_TYPE AS 'Tipo de Dados',
+       COLLATION_NAME AS 'Idioma da Coluna',
+       IS_NULLABLE AS 'Aceita Nulo?'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'FUNCIONARIOS';
+GO
+
+-- Exibe todos os registros da tabela
+SELECT * FROM CLIENTES;
+GO
