ef98c86b8085741e9a57c57ce38388743ed6235f
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-04-25 22:32:13 -0300

    Update Task.java

--- 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/model/Task.java
+++ 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/model/Task.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Task {
 	private Long id;
+	private LocalDate date;
+	private User user;
 	
 	public Long getId() {
 		return id;
@@ -9,5 +13,21 @@ public class Task {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
 	}
 }
