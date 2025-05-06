c907bb2d3715680c6e2584c50c67e9cb86846c61
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-05 22:07:29 -0300

    Update LoginServlet.java

--- 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/servlets/LoginServlet.java
+++ 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/servlets/LoginServlet.java
@@ -1,1 +1,34 @@
+package servlets;
 
+import dao.UserDao;
+import model.User;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private final UserDao userDao = new UserDao();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = userDao.findByEmailAndPassword(email, password);
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("home.jsp");
+        } else {
+            request.setAttribute("error", "Usuário ou senha inválidos.");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+}
