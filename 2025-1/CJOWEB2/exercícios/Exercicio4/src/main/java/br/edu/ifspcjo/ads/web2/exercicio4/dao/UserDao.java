05b6afec27109aae42673e7d775af17190353170
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-05 22:06:21 -0300

    Update UserDao.java

--- 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/dao/UserDao.java
+++ 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/dao/UserDao.java
@@ -1,1 +1,51 @@
+package dao;
 
+import model.User;
+import utils.DataSourceManager;
+
+import java.sql.*;
+
+public class UserDao {
+
+    public void create(User user) {
+        String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+        try (Connection conn = DataSourceManager.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User findByEmailAndPassword(String email, String password) {
+        String sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+        try (Connection conn = DataSourceManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getString("password")
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
