63883031caa6210c9a3d2ef33a0a1d4648666f28
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-05 22:06:42 -0300

    Update Task.java

--- 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/model/Task.java
+++ 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/model/Task.java
@@ -1,33 +1,51 @@
 package model;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 public class Task {
-	private Long id;
-	private LocalDate date;
-	private User user;
-	
-	public Long getId() {
-		return id;
-	}
+    private int id;
+    private String description;
+    private Date date;
+    private User user;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public LocalDate getDate() {
-		return date;
-	}
+    public Task() {}
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	
-	public User getUser() {
-		return user;
-	}
-	
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public Task(int id, String description, Date date, User user) {
+        this.id = id;
+        this.description = description;
+        this.date = date;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
