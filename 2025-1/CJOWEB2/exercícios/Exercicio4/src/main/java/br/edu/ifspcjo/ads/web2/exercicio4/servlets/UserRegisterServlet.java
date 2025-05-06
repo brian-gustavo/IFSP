bdb0e9cae31189d9318e38fbefb52328c9bde329
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-05 22:08:11 -0300

    Update UserRegisterServlet.java

--- 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/servlets/UserRegisterServlet.java
+++ 2025-1/CJOWEB2/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web2/exercicio4/servlets/UserRegisterServlet.java
@@ -1,1 +1,29 @@
+package servlets;
 
+import dao.UserDao;
+import model.User;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class UserRegisterServlet extends HttpServlet {
+
+    private final UserDao userDao = new UserDao();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = new User(0, name, email, password);
+        userDao.create(user);
+
+        response.sendRedirect("login.jsp");
+    }
+}
