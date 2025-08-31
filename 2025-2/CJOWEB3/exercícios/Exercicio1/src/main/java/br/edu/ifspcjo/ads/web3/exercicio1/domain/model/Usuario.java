c89458e373941c3cee088ee699620e1bd5d9fe74
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-08-31 15:00:49 -0300

    Create Usuario.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio1/src/main/java/br/edu/ifspcjo/ads/web3/exercicio1/domain/model/Usuario.java
@@ -0,0 +1,63 @@
+package br.edu.ifspcjo.ads.web3.exercicio1.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+    private boolean ativo;
+    
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
