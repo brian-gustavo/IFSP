39b0858519384b742568b0621d018ac551599197
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-08-31 14:56:37 -0300

    Create Exercicio1ApplicationTests.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio1/src/test/java/br/edu/ifspcjo/ads/web3/exercicio1/Exercicio1ApplicationTests.java
@@ -0,0 +1,11 @@
+package br.edu.ifspcjo.ads.web3.exercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio1ApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
