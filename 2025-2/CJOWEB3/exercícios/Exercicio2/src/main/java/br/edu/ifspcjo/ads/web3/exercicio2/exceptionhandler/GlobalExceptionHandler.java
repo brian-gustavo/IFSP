b74ff1ab184f80ce06333d743b97b53992abdcea
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-07 15:40:12 -0300

    Create GlobalExceptionHandler.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio2/src/main/java/br/edu/ifspcjo/ads/web3/exercicio2/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package br.edu.ifspcjo.ads.web3.exercicio2.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUnknownFieldException(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Atributo desconhecido no corpo da requisição.");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
