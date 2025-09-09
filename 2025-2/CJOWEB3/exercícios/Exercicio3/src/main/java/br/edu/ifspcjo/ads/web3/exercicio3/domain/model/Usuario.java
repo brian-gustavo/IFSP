0c627ae0ee9290178028a454f59c9f64ac49369c
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 20:28:05 -0300

    Create Usuario.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio3/src/main/java/br/edu/ifspcjo/ads/web3/exercicio3/domain/model/Usuario.java
@@ -0,0 +1,78 @@
+package br.edu.ifspcjo.ads.web3.exercicio3.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @Email
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    private String senha;
+    private String telefone;
+    private Boolean ativo = true;
+
+    public Usuario() {
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
