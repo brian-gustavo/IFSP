837da56ad21c12cc7d0d01338407ff8c27ef68a8
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 19:59:16 -0300

    Create Exercicio3Application.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio3/src/main/java/br/edu/ifspcjo/ads/web3/exercicio3/Exercicio3Application.java
@@ -0,0 +1,11 @@
+package br.edu.ifspcjo.ads.web3.exercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio3Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio3Application.class, args);
+	}
+}
