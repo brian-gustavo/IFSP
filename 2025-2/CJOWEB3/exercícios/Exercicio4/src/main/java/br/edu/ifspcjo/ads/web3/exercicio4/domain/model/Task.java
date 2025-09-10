11bc9c6a14cc4c5a95fdff3b82c246ebd733e108
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 21:35:55 -0300

    Create Task.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web3/exercicio4/domain/model/Task.java
@@ -0,0 +1,97 @@
+package br.edu.ifspcjo.ads.web3.exercicio4.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "A descrição da tarefa não pode ser vazia")
+    private String description;
+
+    @NotNull(message = "A data é obrigatória")
+    @FutureOrPresent(message = "A data não pode ser no passado")
+    private LocalDate date;
+
+    private String observation;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O status da tarefa é obrigatório")
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull(message = "A tarefa deve estar associada a um usuário")
+    private User user;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
