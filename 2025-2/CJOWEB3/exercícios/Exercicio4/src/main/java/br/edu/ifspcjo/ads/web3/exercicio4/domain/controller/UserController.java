cb4a36af925cf1833c4805add3221c4d3a2ca932
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 21:37:05 -0300

    Create UserController.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web3/exercicio4/domain/controller/UserController.java
@@ -0,0 +1,57 @@
+package br.edu.ifspcjo.ads.web3.exercicio4.domain.controller;
+
+import br.edu.ifspcjo.ads.web3.exercicio4.domain.model.User;
+import br.edu.ifspcjo.ads.web3.exercicio4.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
+        return userService.findById(id)
+                .map(existingUser -> {
+                    user.setId(id);
+                    User updatedUser = userService.save(user);
+                    return ResponseEntity.ok(updatedUser);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.findById(id).isPresent()) {
+            userService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
