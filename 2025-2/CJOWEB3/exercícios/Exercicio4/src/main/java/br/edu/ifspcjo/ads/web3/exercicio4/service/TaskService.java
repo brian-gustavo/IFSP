b50e8be7159ca3de1178915bd59ebcbb3a00dd5c
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 21:33:10 -0300

    Create TaskService.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web3/exercicio4/service/TaskService.java
@@ -0,0 +1,31 @@
+package br.edu.ifspcjo.ads.web3.exercicio4.service;
+
+import br.edu.ifspcjo.ads.web3.exercicio4.domain.model.Task;
+import br.edu.ifspcjo.ads.web3.exercicio4.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> findById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteById(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
