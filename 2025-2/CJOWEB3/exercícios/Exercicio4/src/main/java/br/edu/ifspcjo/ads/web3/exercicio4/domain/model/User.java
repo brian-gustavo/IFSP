18c005cc9c65a68a712b3a0f3338afd3d9f9639b
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-09-09 21:35:34 -0300

    Create User.java

---
+++ 2025-2/CJOWEB3/exercícios/Exercicio4/src/main/java/br/edu/ifspcjo/ads/web3/exercicio4/domain/model/User.java
@@ -0,0 +1,80 @@
+package br.edu.ifspcjo.ads.web3.exercicio4.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome não pode ser vazio")
+    private String name;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String password;
+
+    private String phone;
+    
+    @Column(name = "is_active")
+    private boolean active;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
