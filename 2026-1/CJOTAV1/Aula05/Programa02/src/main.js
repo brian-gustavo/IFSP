3fa9362e527a216babd308843f83d6c55e059f86
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2026-03-20 20:37:00 -0300

    Add files via upload

---
+++ 2026-1/CJOTAV1/Aula05/Programa02/src/main.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+}
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('logo', 'assets/images/phaser-logo.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'logo');
+}
