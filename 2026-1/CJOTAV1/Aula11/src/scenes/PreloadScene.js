172ba4d595c5d8d6ce09e41022c6dc5e58796142
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2026-05-29 22:06:19 -0300

    Aula

---
+++ 2026-1/CJOTAV1/Aula11/src/scenes/PreloadScene.js
@@ -0,0 +1,59 @@
+import Phaser from 'phaser';
+
+export default class PreloadScene extends Phaser.Scene {
+    constructor() {
+        super({key: 'PreloadScene'});
+    }
+
+    preload() {
+        this.displayProgressBar();
+
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('player', 'assets/images/player.png');
+        this.load.image('idle1', 'assets/images/player01.png');
+        this.load.image('idle2', 'assets/images/player02.png');
+        this.load.image('coca', 'assets/images/coca.png');
+
+        this.load.bitmapFont(
+            'pokelino',
+            'assets/fonts/pokelino/pokelino.png',
+            'assets/fonts/pokelino/pokelino.xml'
+        )
+    }
+
+    create() {
+        this.scene.start('GameScene');
+    }
+
+    displayProgressBar() {
+        const {width, height} = this.cameras.main;
+
+        const progressBarBg = this.add.graphics();
+        progressBarBg.fillStyle(0x222222, 0.8);
+        progressBarBg.fillRect(width / 4 - 2, height / 2 - 12, width / 2 + 4, 24);
+
+        const progressBar = this.add.graphics();
+
+        const loadingText = this.add.text(
+            width / 2,
+            height / 2 - 30,
+            'Loading...',
+            {
+                fontSize: '20px',
+                fill: '#ffffff'
+            }
+        ).setOrigin(0.5);
+
+        this.load.on('progress', (value) => {
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(width / 4 - 2, height / 2 - 10, (width / 2) * value, 20);
+        });
+
+        this.load.on('complete', () => {
+            progressBar.destroy();
+            progressBarBg.destroy();
+            loadingText.destroy();
+        });
+    }
+}
